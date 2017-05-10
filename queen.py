94666ec03078a2e800e04413c0393af73f62d9a6
seiya <hoge@hoge>
2017-05-10 22:23:43 +0900

    fix bugs

--- queen.py
+++ queen.py
@@ -1,22 +1,27 @@
 import operator as op
-from functools import reduce
-from itertools import chain
+from itertools import chain, islice
+from time import time
 
 def main():
-    for ans in solve():
+    num_solution = 100
+    t = time()
+    for i, ans in enumerate(islice(solve(8), num_solution)):
         show(ans)
-        break
+        print('total {}s'.format(time() - t))
+        print('aberage {}s'.format((time() - t) / (i+1)))
+
 
 def solve(length=8):
     def valid(x, y, queens):
-        return reduce(op.and_, (not(xq==x or yq==y or xq-yq==x-y or xq+yq==x+y) for xq, yq in queens), True)
+        return all(not(xq==x or yq==y or xq-yq==x-y or xq+yq==x+y) for xq, yq in queens)
 
     def solve_row(i_row, queens):
         if i_row==length:
             yield queens
         else:
-            for y in range(length):
-                yield from chain(*(solve_row(i_row+1, queens + [(i_row, y)]) for y in range(length) if valid(i_row, y, queens)))
+            valid_ys = (y for y in range(length) if valid(i_row, y, queens))
+            answers = (solve_row(i_row+1, queens + [(i_row, y)]) for y in valid_ys)
+            yield from chain.from_iterable(answers)
     yield from solve_row(0, [])
 
 
@@ -24,7 +29,7 @@ def show(queens):
     sorted_queens = sorted(queens, key=lambda x: x[1])
     for queen in sorted_queens:
         x, _ = queen
-        row = '.'*x + '*' + '.'*(len(sorted_queens)-x-1)
+        row = '. '*x + '* ' + '. '*(len(sorted_queens)-x-1)
         print(row)
 
 if __name__ == '__main__':
