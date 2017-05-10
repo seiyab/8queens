9544a938a63f298b8f0706cfbfc3ec2d889e2638
seiya <hoge@hoge>
2017-05-10 22:49:01 +0900

    add comments

--- queen.py
+++ queen.py
@@ -12,10 +12,21 @@ def main():
 
 
 def solve(length):
+    """length: int -> iter<[(int, int)]>
+    solve length x length queen problem.
+    each element of return: a solution for the problem.
+    each element of a solution: a coordinate of a queen.
+    """
     def valid(x, y, queens):
-        return all(not(xq==x or yq==y or xq-yq==x-y or xq+yq==x+y) for xq, yq in queens)
+        """x: int -> y: int -> queens: [(int, int)] -> bool
+        whether we can put a queen at (x, y) when queens are already put.
+        """
+        return all(xq!=x and yq!=y and xq-yq!=x-y and xq+yq!=x+y for xq, yq in queens)
 
     def solve_row(i_row, queens):
+        """i_row: int -> queens: [(int, int)] -> iter<[(int, int)]>
+        solve length x length queenproblem assuming row y is already filled by "queens" for y<"irow".
+        """
         if i_row==length:
             yield queens
         else:
